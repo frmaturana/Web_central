677d367bb2aa650751783b16b28deaa18b76ad46
Sebastián Andrés Durán Vilches <sduran.vilches@gmail.com>
2024-08-05 09:32:25 -0400

    astro github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fmaturana.cl/',
+    site: 'https://latsa.cl/',
     base: '/Web_central',
 });
